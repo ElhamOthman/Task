335fe72814106e67e228f71b408b15726eb92c1d
ElhamOthman <30947018+ElhamOthman@users.noreply.github.com>
2023-06-08 13:59:14 +0300

    Update manager.php

--- manager.php
+++ manager.php
@@ -14,33 +14,7 @@ class Manager
         $this->students = [];
     }
 
-    public function addStudent(Student $student)
-    {
-        $this->students[$student->getId()] = $student;
-        $this->log('Added student: ' . $student->getName());
-    }
-
-    public function getStudent(string $id): ?Student
-    {
-        return $this->students[$id] ?? null;
-    }
-
-    public function updateStudent(Student $student)
-    {
-        if (isset($this->students[$student->getId()])) {
-            $this->students[$student->getId()] = $student;
-            $this->log('Updated student: ' . $student->getName());
-        }
-    }
-
-    public function deleteStudent(string $id)
-    {
-        if (isset($this->students[$id])) {
-            $name = $this->students[$id]->getName();
-            unset($this->students[$id]);
-            $this->log('Deleted student: ' . $name);
-        }
-    }
+   
 
 }
 
